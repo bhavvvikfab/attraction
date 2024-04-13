b13ba95cd1d815918aa76f2fdab5a5f8773630f7
Arun <arunfablead@gmail.com>
2024-04-13 11:59:59 +0530

    update topup

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('/', function(){ return redirect('/agent'); });
 
 // route's for ADMIN start
 Route::prefix('admin')->middleware([Auther::class])->group(function () {
-    Route::get('/logout', [LoginController::class, 'logout']);
+    Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
     Route::post('/loginporcess', [LoginController::class, 'login']);
     Route::post('/register', [LoginController::class, 'register']);
     Route::get('/login', [LoginController::class, 'index']);
@@ -49,6 +49,7 @@ Route::prefix('admin')->middleware([Auther::class])->group(function () {
 
 
     Route::get('/topup', [TopupController::class, 'index'])->name('admin.topup');
+    Route::post('/update-topup-status', [TopupController::class, 'updateTopUpStatus']);
 
     Route::get('/admin_invoice', [InvoiceController::class, 'index'])->name('admin.admin_invoice');
     Route::get('/view_single_invoice', [InvoiceController::class, 'view_single_invoice'])->name('admin.view_single_invoice');
@@ -56,19 +57,17 @@ Route::prefix('admin')->middleware([Auther::class])->group(function () {
 
     Route::get('/all_chat', [ChatController::class, 'index'])->name('admin.all_chat');
     Route::get('/view_chat', [ChatController::class, 'view_chat'])->name('admin.view_chat');
-    Route::get('/topup', [TopupController::class, 'index']);
-    Route::post('/update-topup-status', [TopupController::class, 'updateTopUpStatus']);
 });
 // route's for ADMIN end
 
 // route's for AGENT start
 Route::prefix('agent')->middleware([Auther::class])->group(function () {
-    Route::get('/logout', [LoginController::class, 'logout']);
+    Route::get('/logout', [LoginController::class, 'logout'])->name('agent.logout');
     Route::post('/loginporcess', [LoginController::class, 'login']);
     Route::post('/register', [LoginController::class, 'register']);
     Route::get('/login', [LoginController::class, 'index']);
     Route::get('/register', [LoginController::class, 'register_page']);
-    Route::get('/admin_profile', [LoginController::class, 'admin_profile']);
+    Route::get('/admin_profile', [LoginController::class, 'admin_profile'])->name('agent.admin_profile');
 
     Route::get('/', function () {
         $data['authUser'] = Auth::user();
@@ -76,10 +75,20 @@ Route::prefix('agent')->middleware([Auther::class])->group(function () {
         return view('index');
     })->name('agent');
 
-    Route::get('/all_attraction', [AttractionController::class, 'index']);
-    Route::get('/view_attraction', [AttractionController::class, 'view_attraction']);
+    Route::get('/all_attraction', [AttractionController::class, 'index'])->name('agent.all_attraction');
+    Route::get('/view_attraction', [AttractionController::class, 'view_attraction'])->name('agent.view_attraction');
 
-    Route::get('/topup', [TopupController::class, 'index']);
+    Route::get('/topup', [TopupController::class, 'index'])->name('agent.topup');
     Route::post('/request-topup', [TopupController::class, 'requestTopUp']);
+
+    Route::get('/all_booking', [AttractionController::class, 'index'])->name('agent.all_booking');
+
+    Route::get('/all_agent', [AttractionController::class, 'view_attraction'])->name('agent.all_agent');
+    Route::get('/add_agent', [AttractionController::class, 'view_attraction'])->name('agent.add_agent');
+
+    Route::get('/admin_invoice', [AttractionController::class, 'view_attraction'])->name('agent.admin_invoice');
+    
+    Route::get('/all_chat', [AttractionController::class, 'view_attraction'])->name('agent.all_chat');
+
 });
 // route's for AGENT end
