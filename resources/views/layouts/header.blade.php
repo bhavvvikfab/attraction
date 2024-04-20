73fcefa8095860c820b7b2f2bc79353a913a2c2d
Bhavik <bhavik.fablead@gmail.com>
2024-04-20 14:11:52 +0530

    profile image set default

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -94,7 +94,8 @@
         <li class="nav-item dropdown pe-3 ps-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="{{ asset('assets/img/' . (!empty(Auth::user()->profile) ? Auth::user()->profile : 'default.jpg')) }}" alt="Profile" class="rounded-circle">
+          <?php $defalut_profile = asset('assets/img/default.jpg'); ?>
+          <img src="{{ asset('assets/img/' . (!empty(Auth::user()->profile) ? Auth::user()->profile : 'default.jpg')) }}" onerror="this.onerror=null; this.src='{{$defalut_profile}}'" alt="Profile" class="rounded-circle">
           
           <span class="d-none d-md-block dropdown-toggle ps-2">{{Auth::user()->name}}</span>
           </a><!-- End Profile Iamge Icon -->
